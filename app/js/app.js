fdbff78bd9abd394f44d5c75818c4a6ffc75d658
bradbitz11 <bitzerb@gmail.com>
2023-07-06 21:55:30 -0400

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete','OrderCloud-SpecForms','OrderCloud-Carousel','OrderCloud-ProductSearchInput','OrderCloud-Minicart','OrderCloud-ProductLightbox','OrderCloud-ProductZoom','OrderCloud-Breadcrumbs','OrderCloud-SameAsShippingCheckbox']);
