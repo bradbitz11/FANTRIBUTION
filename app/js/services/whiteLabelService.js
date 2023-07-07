18e18661ed4756ce5c5d37a65c9cedab88343ae9
bradbitz11 <bitzerb@gmail.com>
2023-07-06 21:59:20 -0400

    Update whiteLabelService.js

--- app/js/services/whiteLabelService.js
+++ app/js/services/whiteLabelService.js
@@ -1,4 +1,1 @@
-four51.app.factory('WhiteLabel', function() {
-	var replacements = [];
-	return { replacements: replacements };
-});
+four51.app.factory('WhiteLabel', function() { var replacements = [{"key":"Promotion","value":"Participant"},{"key":"Coupon","value":"Participant Code"}]; return { replacements: replacements }; });
